ec6329f6dce8da67aaa312b9e388b3196ed07860
juancisneros <juanfrancistm2011@icloud.com>
2023-03-07 17:52:48 -0500

    creacion de un nuevo usuario. - socket funcional (mediante post /api/login/new) - db guarda el usuario (mongo DB guarda al usuario en la base de datos) --> email, contrasena y nombre ->> se requiere encriptar el password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,46 @@
 const {response} = require('express');
-const crearUsuario = (req,res=response) => {
-    res.json({
-        ok: true,
-        msg: 'Crear usuario!!!'
-    })
+const { validationResult, body } = require('express-validator');
+const Usuario = require('../models/usuario');
+
+const crearUsuario = async (req,res=response) => {
+
+    const {email} = req.body;
+
+    try{
+
+        const existeEmail = await Usuario.findOne({email});
+        if (existeEmail){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya esta registrado'
+            });
+        }
+
+        const usuario = new Usuario(req.body);
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            msg: 'Crear usuario!!!',
+            usuario, 
+        })
+
+
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
+
+
+
+
+
+
 }
 
 module.exports = {
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,18 @@
 
 const {crearUsuario} = require("../controllers/auth");
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require("../middlewares/validar_campos");
 const router = Router();
 
-router.post('/new', crearUsuario);
+router.post('/new', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    validarCampos,
+    check('email', 'El email es obligatorio').isEmail(),
+    validarCampos,
+    check('password', 'El password debe ser de 6 caracteres').isLength({min:6}),
+    validarCampos
+],crearUsuario);
 
 
 
