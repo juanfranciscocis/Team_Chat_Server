0abe3f5d616c41fd5957ed811f43ae59f3d751b4
juancisneros <juanfrancistm2011@icloud.com>
2023-03-14 19:27:46 -0500

    AUTENTICO AL CLIENTE DEL LADO DE LOS SOCKETS - EN CASO DE NO PROVEER JSON TOKEN EL USUARIO NO PUEDE INGRESAR

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,21 @@
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
+
+    //console.log(client.handshake.headers['x-token']);
+    
+    const[valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    if(!valido){
+        console.log('Cliente no autenticado');
+        return client.disconnect();
+    }
     console.log('Cliente conectado');
+    console.log(valido, uid);
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
